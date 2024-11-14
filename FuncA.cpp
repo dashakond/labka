2270fde2c4df9197b778900a9c1a555020ab7156
daryna <dasha.kondratez@gmail.com>
2024-11-14 16:05:48 +0200

    Hotfix: calculate function for first 3 elements

--- FuncA.cpp
+++ FuncA.cpp
@@ -4,9 +4,10 @@
 
 double FuncA::calculate(int n, double x) {
     double result = 0.0;
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < 3; ++i) {
         result += pow(-1, i) * pow(x, 2 * i) / tgamma(2 * i + 1);
     }
     return result;
 }
+
 
